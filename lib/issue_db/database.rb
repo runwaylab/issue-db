15826c5b6bc9cb70bf3c0c3e4f616c6272daec42
GrantBirki <grant.birkinbine@gmail.com>
2024-11-04 17:00:51 -0800

    scaffold database

---
+++ lib/issue_db/database.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+# class DatabaseError < StandardError; end
+
+class Database
+  def initialize(log, client)
+    @log = log
+    @client = client
+  end
+
+  def read
+
+  end
+
+  def write
+
+  end
+
+  def delete
+
+  end
+
+  def list
+
+  end
+end

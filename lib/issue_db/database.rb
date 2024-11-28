ee2b4dda1a8052bed6cd814bd9a86100611b643c
GrantBirki <grant.birkinbine@gmail.com>
2024-11-27 16:11:09 -0800

    keep it DRY with `find_issue_by_key` method

--- lib/issue_db/database.rb
+++ lib/issue_db/database.rb
@@ -26,16 +26,13 @@ class Database
   def create(key, data, options = {})
     @log.debug("attempting to create: #{key}")
 
-    body = generate(data, body_before: options[:body_before], body_after: options[:body_after])
-
-    issues.each do |issue|
-      # if there is an exact match and the issue is open, we found a match
-      # if include_closed is true, we will include closed issues (all types) in the search
-      next unless issue[:title] == key && ((options[:include_closed]) || issue[:state] == "open")
-
+    issue = find_issue_by_key(key, options)
+    if issue
       @log.warn("skipping issue creation and returning existing issue - an issue already exists with the key: #{key}")
-      return Record.new(issue)
+      return issue
     end
+
+    body = generate(data, body_before: options[:body_before], body_after: options[:body_after])
 
     # if we make it here, no existing issues were found so we can safely create one
     issue = Retryable.with_context(:default) do
@@ -51,18 +48,7 @@ class Database
 
   def read(key, options = {})
     @log.debug("attempting to read: #{key}")
-
-    issues.each do |issue|
-      # if there is an exact match and the issue is open, we found a match
-      # if include_closed is true, we will include closed issues (all types) in the search
-      next unless issue[:title] == key && ((options[:include_closed]) || issue[:state] == "open")
-
-      return Record.new(issue)
-    end
-
-    # if we make it here, no issue was found in the cache for the given key (title)
-    @log.debug("no issue found in cache for: #{key}")
-    return nil
+    return find_issue_by_key(key, options)
   end
 
   def update
@@ -82,6 +68,24 @@ class Database
   end
 
   protected
+
+  # A helper method to search through the issues cache and return the first issue that matches the given key
+  # :param: key [String] the key (issue title) to search for
+  # :param: options [Hash] a hash of options to pass through to the search method
+  # :return: The issue as a Record object if found, otherwise nil
+  def find_issue_by_key(key, options = {})
+    issues.each do |issue|
+      # if there is an exact match and the issue is open, we found a match
+      # if include_closed is true, we will include closed issues (all types) in the search
+      next unless issue[:title] == key && ((options[:include_closed]) || issue[:state] == "open")
+
+      return Record.new(issue)
+    end
+
+    # if we make it here, no issue was found in the cache for the given key (title)
+    @log.debug("no issue found in cache for: #{key}")
+    return nil
+  end
 
   def issues
     # update the issues cache if it is nil

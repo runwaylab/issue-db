ba26c3da0bc81606a8958f8bfc4f010221b6df52
GrantBirki <grant.birkinbine@gmail.com>
2025-07-31 10:29:36 -0700

    support github app auth from a pass through from the entry point of the IssueDB client

--- lib/issue_db/authentication.rb
+++ lib/issue_db/authentication.rb
@@ -7,22 +7,28 @@ module IssueDB
   class AuthenticationError < StandardError; end
 
   module Authentication
-    def self.login(client = nil, log = nil)
+    def self.login(client = nil, log = nil, app_id: nil, installation_id: nil, app_key: nil, app_algo: nil)
       # if the client is not nil, use the pre-provided client
       unless client.nil?
         log.debug("using pre-provided client") if log
         return client
       end
 
-      # if the client is nil, check for GitHub App env vars first
+      # if GitHub App parameters are provided, use them first
+      if app_id && installation_id && app_key
+        log.debug("using provided github app authentication parameters") if log
+        return IssueDB::Utils::GitHub.new(log:, app_id:, installation_id:, app_key:, app_algo:)
+      end
+
+      # if the client is nil, check for GitHub App env vars
       # first, check if all three of the following env vars are set and have values
       # ISSUE_DB_GITHUB_APP_ID, ISSUE_DB_GITHUB_APP_INSTALLATION_ID, ISSUE_DB_GITHUB_APP_KEY
-      app_id = ENV.fetch("ISSUE_DB_GITHUB_APP_ID", nil)
-      installation_id = ENV.fetch("ISSUE_DB_GITHUB_APP_INSTALLATION_ID", nil)
-      app_key = ENV.fetch("ISSUE_DB_GITHUB_APP_KEY", nil)
-      if app_id && installation_id && app_key
-        log.debug("using github app authentication") if log
-        return IssueDB::Utils::GitHub.new(log:, app_id:, installation_id:, app_key:)
+      env_app_id = ENV.fetch("ISSUE_DB_GITHUB_APP_ID", nil)
+      env_installation_id = ENV.fetch("ISSUE_DB_GITHUB_APP_INSTALLATION_ID", nil)
+      env_app_key = ENV.fetch("ISSUE_DB_GITHUB_APP_KEY", nil)
+      if env_app_id && env_installation_id && env_app_key
+        log.debug("using github app authentication from environment variables") if log
+        return IssueDB::Utils::GitHub.new(log:, app_id: env_app_id, installation_id: env_installation_id, app_key: env_app_key)
       end
 
       # if the client is nil and no GitHub App env vars were found, check for the ISSUE_DB_GITHUB_TOKEN
